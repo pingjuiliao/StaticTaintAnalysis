bc3bea6868ae265b1b2634cbe3791b9383b50bd9
pingjui <pingjuiliao@gmail.com>
2021-05-29 19:32:15 -0700

    add samples, scripts, gitignore

---
+++ build/CMakeFiles/BackwardSlicing.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pingjui/StaticTaintAnalysis/script/pass.cpp" "/home/pingjui/StaticTaintAnalysis/build/CMakeFiles/BackwardSlicing.dir/script/pass.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/pingjui/BasicBlockIDTracing/llvm-project-10.0.1/llvm/include"
+  "/home/pingjui/BasicBlockIDTracing/llvm-project-10.0.1/build/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
